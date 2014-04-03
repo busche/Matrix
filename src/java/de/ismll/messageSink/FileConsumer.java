78f9103362abb38acac447f1653c978c18b1fa20
André Busche <andre.busche@gmx.de>
2014-04-03 13:13:00 +0200

    introduced logger

--- src/java/de/ismll/messageSink/FileConsumer.java
+++ src/java/de/ismll/messageSink/FileConsumer.java
@@ -6,7 +6,12 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.lang.ref.WeakReference;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 public class FileConsumer implements MessageConsumer {
+
+	protected Logger logger = LogManager.getLogger(getClass());
 
 	private File file;
 	private WeakReference<PrintStream> ref;
@@ -43,7 +48,7 @@ public class FileConsumer implements MessageConsumer {
 			ref = new WeakReference<PrintStream>(printStream);
 			ret = printStream;
 		} else {
-			System.out.println("recycling PrintStream");
+			logger.debug("recycling PrintStream");
 		}
 
 		return ret;
